a3c60b0dbb1252c6855cb5dffd531d59b162ee46
David Li <davidli012345@gmail.com>
2021-02-17 21:01:18 -0800

    pulumi configurated correctly

--- main.go
+++ main.go
@@ -2,49 +2,52 @@ package main
 
 import (
 	"github.com/pulumi/pulumi-gcp/sdk/v4/go/gcp/cloudfunctions"
-	// "github.com/pulumi/pulumi-gcp/sdk/v4/go/gcp/storage"
+	"github.com/pulumi/pulumi-gcp/sdk/v4/go/gcp/storage"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/FriendlyUser/cooking-app/types"
+	"path/filepath"
 	"fmt"
 )
 
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// LEGACY
-		// Create a bucket.
-		// bucket, err := storage.NewBucket(ctx, "bucket", nil)
-		// if err != nil {
-		// 	return err
-		// }
+		bucket, err := storage.NewBucket(ctx, "bucket", nil)
+		if err != nil {
+			return err
+		}
+		// loop through function config
+		p := filepath.FromSlash("functions/helloworld")
+		// Create an object in our bucket with our function.
+		bucketObjectArgs := &storage.BucketObjectArgs{
+			Bucket: bucket.Name,
+			Source: pulumi.NewFileArchive(p),
+		}
+		bucketObject, err := storage.NewBucketObject(ctx, "helloworld-zip", bucketObjectArgs)
+		if err != nil {
+			return err
+		}
+		cfg := &types.FuncConfig{
 
-		// // Create an object in our bucket with our function.
-		// bucketObjectArgs := &storage.BucketObjectArgs{
-		// 	Bucket: bucket.Name,
-		// 	Source: pulumi.NewFileArchive("functions"),
-		// }
-		// bucketObject, err := storage.NewBucketObject(ctx, "go-zip", bucketObjectArgs)
-		// if err != nil {
-		// 	return err
-		// }
-
+		}
+		if cfg == nil {
+			fmt.Println(cfg)
+		}
 		// load files from cloud source repository
 		// Set arguments for creating the function resource.
-		fmt.Println("Failing here")
 		args := &cloudfunctions.FunctionArgs{
 			Runtime:             pulumi.String("dotnet3"),
-			SourceRepository: &cloudfunctions.FunctionSourceRepositoryArgs{
-				Url: pulumi.String("https://source.cloud.google.com/cooking-app-dli/github_friendlyuser_cooking-app"),
-			},
-			// SourceRepository: &cloudfunctions.FunctionSourceRepositoryArgs{
-			// 	Url: "https://source.cloud.google.com/cooking-app-dli/github_friendlyuser_cooking-app?authuser=0",
-			// },
-			EntryPoint:          pulumi.String("HandleAsync"),
+			SourceArchiveBucket: bucket.Name,
+			SourceArchiveObject: bucketObject.Name,
+			// https://codelabs.developers.google.com/codelabs/cloud-functions-csharp#3
+			EntryPoint:          pulumi.String("HelloHttp.Function"),
 			TriggerHttp:         pulumi.Bool(true),
 			AvailableMemoryMb:   pulumi.Int(128),
 		}
-
+// cooking-app-dli/github_friendlyuser_cooking-app/main/functions/helloworld/Function.cs
 
 		// Create the function using the args.
-		function, err := cloudfunctions.NewFunction(ctx, "basicFunction", args)
+		function, err := cloudfunctions.NewFunction(ctx, "HelloHttp", args)
 		if err != nil {
 			return err
 		}
---
+++ types/types.go
@@ -0,0 +1,8 @@
+package types
+
+type FuncConfig struct {
+    // path for 
+    FolderPath string
+    EntryPoint  int
+    AvailableMemoryMb int
+}
